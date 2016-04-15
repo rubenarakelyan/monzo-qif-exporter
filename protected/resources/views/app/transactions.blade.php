1e85adc598955a50da6a85cb925ea06b149222b8
Ruben Arakelyan <ruben@ra.me.uk>
2016-04-16 00:05:07 +0100

    Improve transactions page

--- protected/resources/views/app/transactions.blade.php
+++ protected/resources/views/app/transactions.blade.php
@@ -5,13 +5,14 @@
 @section('content')
 
       <div class="row">
-	    <div class="col-md-12">
+	    <div class="col-md-8 col-md-offset-2">
           @if ($transactions === null)
           <p>There are no transactions to display.</p>
           @else
           <table class="table table-hover">
 	        <thead>
 	          <tr>
+		        <th></th>
 		        <th>Merchant name</th>
 		        <th class="text-right">Amount</th>
 	          </tr>
@@ -19,7 +20,8 @@
 	        <tbody>
 	          @foreach ($transactions as $transaction)
 	          <tr>
-		        <td>@if (!empty($transaction->merchant->logo)) <img src="{{ $transaction->merchant->logo }}" width="30" height="30" alt=""> @endif @if (!empty($transaction->merchant->name)) {{ $transaction->merchant->name }} @else {{ $transaction->description }} @endif</td>
+		        <td>@if (!empty($transaction->merchant->logo)) <img src="{{ $transaction->merchant->logo }}" width="30" height="30" alt=""> @endif</td>
+		        <td>@if (!empty($transaction->merchant->name)) {{ $transaction->merchant->name }} @else {{ $transaction->description }} @endif</td>
 		        <td class="text-right @if (($transaction->amount / 100) < 0) debit @else credit @endif">{{ Currency::withPrefix(abs($transaction->amount / 100), $transaction->currency, 2) }}</td>
 	          </tr>
 	          @endforeach
