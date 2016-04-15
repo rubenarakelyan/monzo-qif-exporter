b38a49da4d4083af79942d6969a6a3ca47faed92
Ruben Arakelyan <ruben@ra.me.uk>
2016-04-16 00:07:50 +0100

    Add transaction date/time

--- protected/resources/views/app/transactions.blade.php
+++ protected/resources/views/app/transactions.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
       <div class="row">
-	    <div class="col-md-8 col-md-offset-2">
+	    <div class="col-md-10 col-md-offset-1">
           @if ($transactions === null)
           <p>There are no transactions to display.</p>
           @else
@@ -13,7 +13,8 @@
 	        <thead>
 	          <tr>
 		        <th></th>
-		        <th>Merchant name</th>
+		        <th>Date/Time</th>
+		        <th>Merchant</th>
 		        <th class="text-right">Amount</th>
 	          </tr>
 	        </thead>
@@ -21,6 +22,7 @@
 	          @foreach ($transactions as $transaction)
 	          <tr>
 		        <td>@if (!empty($transaction->merchant->logo)) <img src="{{ $transaction->merchant->logo }}" width="30" height="30" alt=""> @endif</td>
+		        <td>{{ (new \DateTime($transaction->created))->format('j M Y H:i') }}</td>
 		        <td>@if (!empty($transaction->merchant->name)) {{ $transaction->merchant->name }} @else {{ $transaction->description }} @endif</td>
 		        <td class="text-right @if (($transaction->amount / 100) < 0) debit @else credit @endif">{{ Currency::withPrefix(abs($transaction->amount / 100), $transaction->currency, 2) }}</td>
 	          </tr>
